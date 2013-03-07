430697131c018b21fa7a06fa5842e37062c5c774
Jay Patel <jayhp9@stanford.edu>
2013-03-07 03:30:10 -0800

    added dict

---
+++ src/MTHindi.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+
+public class MTHindi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private HashMap<String, String> generateDict(){
+		HashMap<String, String> dict = new HashMap<String, String>();
+		
+	}
+
+}
